625a92babf2657cedc563c3c2e3a646d440b5901
mrSoftIce <mr.softice@gmail.com>
2025-12-18 18:06:08 +0400

    Django ToolBar embedded

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,8 @@
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from foodgram import settings
 
 urlpatterns = [
     path('api/', include('api.urls')),
@@ -10,3 +11,9 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    print('Debug toolbar enabled')
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
