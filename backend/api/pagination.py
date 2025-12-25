1fb3b32b7d6a9c8359afb05664cf2dccff54f982
mrSoftIce <mr.softice@gmail.com>
2025-12-25 09:32:20 +0400

    bugfix: pagination не правильно был выбран тип пагинации. LimitOffsetPagination заменил на PageNumberPagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,11 +1,8 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class PageLimitPagination(LimitOffsetPagination):
+class PageLimitPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_query_param = 'page'
-    default_limit = 10
+    default_limit = 6
     max_page_size = 100
-
-    def get_offset(self, request):
-        return 0
