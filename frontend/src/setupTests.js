29c2739abf439d7a4a8b31fccfe30034948aabe2
mrSoftIce <mr.softice@gmail.com>
2026-01-09 17:31:00 +0400

    Front end tests fall on githab Actions

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// CRA runs this file before each test suite
+
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+}
+
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = (id) => clearTimeout(id);
+}
