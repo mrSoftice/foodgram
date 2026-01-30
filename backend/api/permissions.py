02406b689f7c4625434c9ba727d96a224c78c65e
mrSoftIce <mr.softice@gmail.com>
2026-01-30 14:25:48 +0400

    review 1.2

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,25 +9,7 @@ class IsAuthorOrReadOnly(BasePermission):
     - PUT/PATCH/DELETE разрешены только авторам рецептов.
     """
 
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        # создание и удаление (POST, DELETE)
-        # доступно только аутентифицированным пользователям
-        if request.method == 'POST':
-            return request.user and request.user.is_authenticated
-        # остальные методы (PUT, PATCH, DELETE) мы передаем
-        # в has_object_permission
-        return True
-
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        author = getattr(obj, 'author', None)
-        if author is None:
-            return False
-        return (
-            request.user
-            and request.user.is_authenticated
-            and author == request.user
-        )
+        return obj.author == request.user
