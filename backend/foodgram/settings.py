af674127b12ea1c56835b1d9c2fa72efdf67d232
mrSoftIce <mr.softice@gmail.com>
2026-02-06 13:09:12 +0400

    Wrong default serializer for user creation

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -163,7 +163,6 @@ DJOSER = {
     },
     'SERIALIZERS': {
         'user': 'api.serializers.UserSerializer',
-        'user_create': 'api.serializers.UserCreateSerializer',
         'current_user': 'api.serializers.UserSerializer',
     },
 }
